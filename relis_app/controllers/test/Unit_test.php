68a2f6b219975979e5af9826fc870d62bcd3fb22
christmalak90 <christ.malak@gmail.com>
2023-12-31 15:49:21 -0500

    check onpush action69

--- relis_app/controllers/test/Unit_test.php
+++ relis_app/controllers/test/Unit_test.php
@@ -109,8 +109,8 @@ class Unit_test extends CI_Controller
         // $this->screeningUnitTest->run_tests();
         // $this->quality_assessmentUnitTest->run_tests();
         // $this->data_extractionUnitTest->run_tests();
-        $this->reportingUnitTest->run_tests();
-        // $this->elementUnitTest->run_tests(); 
+        // $this->reportingUnitTest->run_tests();
+        $this->elementUnitTest->run_tests(); 
         // $this->homeUnitTest->run_tests();
         // $this->installUnitTest->run_tests();
         // $this->adminUnitTest->run_tests();

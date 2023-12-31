d70f5d812cfa8fcb487e41e78f40326963273d6a
christmalak90 <christ.malak@gmail.com>
2023-12-31 06:07:07 -0500

    check onpush action60

--- relis_app/controllers/test/Unit_test.php
+++ relis_app/controllers/test/Unit_test.php
@@ -103,10 +103,10 @@ class Unit_test extends CI_Controller
         // Record the start time of the tests
         $startTime = microtime(true);
 
-        // $this->userUnitTest->run_tests();
-        // $this->projectUnitTest->run_tests();
-        // $this->paperUnitTest->run_tests();
-        // $this->screeningUnitTest->run_tests();
+        $this->userUnitTest->run_tests();
+        $this->projectUnitTest->run_tests();
+        $this->paperUnitTest->run_tests();
+        $this->screeningUnitTest->run_tests();
         $this->quality_assessmentUnitTest->run_tests();
         // $this->data_extractionUnitTest->run_tests();
         // $this->reportingUnitTest->run_tests();

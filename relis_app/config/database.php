3a128c2e5bb509988173bd6324b69938884e07ba
christmalak90 <christ.malak@gmail.com>
2023-12-31 19:39:00 -0500

    check onpush action87

--- relis_app/config/database.php
+++ relis_app/config/database.php
@@ -205,6 +205,50 @@ $db['demo_relis'] = array(
 );
 
 
+$db['demoTestProject'] = array(
+		'dsn'	=> '',
+		'hostname' => 'db',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'relis_dev_correct_demoTestProject',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => (ENVIRONMENT !== 'production'),
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
+);
+
+$db['demoTestProject'] = array(
+		'dsn'	=> '',
+		'hostname' => 'db',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'relis_dev_correct_demoTestProject',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => (ENVIRONMENT !== 'production'),
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
+);
+
 $db['demoTestProject2'] = array(
 		'dsn'	=> '',
 		'hostname' => 'db',
@@ -226,7 +270,6 @@ $db['demoTestProject2'] = array(
 		'failover' => array(),
 		'save_queries' => TRUE
 );
-
 
 $db['demoTestProject'] = array(
 		'dsn'	=> '',
